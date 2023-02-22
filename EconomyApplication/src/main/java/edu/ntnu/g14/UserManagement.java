5ea392ef2ba0d3d8f4170229112a926c6d3822de
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2023-02-22 09:16:14 +0100

    created UserManagement constructor with methods to create new user, remove user and verify user.

--- EconomyApplication/src/main/java/edu/ntnu/g14/UserManagement.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/UserManagement.java
@@ -1,4 +1,23 @@
 package edu.ntnu.g14;
+import java.util.HashMap;
+
 public class UserManagement {
-    
+    private HashMap<String, User> users;
+
+    public UserManagement() {
+        this.users = new HashMap<>();
+    }
+
+    public void createNewUser(String userId, HashMap<String, Account> accounts, HashMap<String, Invoice> invoices) {
+        User user = new User(accounts, invoices, userId);
+        users.put(userId, user);
+    }
+
+    public void removeUser(String userId) {
+        users.remove(userId);
+    }
+
+    public boolean verifyUser(String userId) {
+        return users.containsKey(userId);
+    }
 }
