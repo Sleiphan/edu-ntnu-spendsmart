6d3e789d71de5da8ae3af64279ebc39e097bc7b1
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2023-02-22 09:14:35 +0100

    Added user constructor and methods to receive accounts, invoices and userId

--- EconomyApplication/src/main/java/edu/ntnu/g14/User.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/User.java
@@ -1,4 +1,28 @@
 package edu.ntnu.g14;
-public class User {
-    
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+public class User extends Personalia {
+    private HashMap<String, Account> accounts;
+    private HashMap<String, Invoice> invoices;
+    private String userId;
+
+    public User(HashMap<String, Account> accounts,HashMap<String, Invoice> invoices, String userId ) {
+        this.accounts=accounts;
+        this.invoices=invoices;
+        this.userId=userId;
+        // personalia object?
+    }
+
+    public List<Account> getAllAccounts() {
+        return new ArrayList<>(accounts.values());
+    }
+    public List<Invoice> getAllInvoices() {
+        return new ArrayList<>(invoices.values());
+    }
+
+    public String getUserId() {
+        return userId;
+    }
 }
