22752eb7794c7c2b82a274dc91d0365a082f1288
Isak Kallestad Mandal <isakkm@stud.ntnu.no>
2023-02-22 09:40:17 +0100

    Created Transaction class with getters and setters

--- EconomyApplication/src/main/java/edu/ntnu/g14/Transaction.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/Transaction.java
@@ -1,4 +1,72 @@
 package edu.ntnu.g14;
+
+import java.util.Date;
+
 public class Transaction {
-    
+
+  //These private fields should maybe be public instead?
+    private String fromAccountId;
+    private String toAccountId;
+    private short amount;
+    private String description;
+    private Date dateOfTransaction;
+
+    public Transaction(String fromAccountId, String toAccountId, short amount, String description, Date dateOfTransaction) {
+        this.fromAccountId = fromAccountId;
+        this.toAccountId = toAccountId;
+        this.amount = amount;
+        this.description = description;
+        this.dateOfTransaction = dateOfTransaction;
+    }
+
+    public String getFromAccountId() {
+        return fromAccountId;
+    }
+
+    public String getToAccountId() {
+        return toAccountId;
+    }
+
+    public short getAmount() {
+        return amount;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Date getDateOfTransaction() {
+        return dateOfTransaction;
+    }
+
+    public void setFromAccountId(String fromAccountId) {
+        this.fromAccountId = fromAccountId;
+    }
+
+    public void setToAccountId(String toAccountId) {
+        this.toAccountId = toAccountId;
+    }
+
+    public void setAmount(short amount) {
+        this.amount = amount;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setDateOfTransaction(Date dateOfTransaction) {
+        this.dateOfTransaction = dateOfTransaction;
+    }
+
+    public Transaction newTransaction() {
+        return new Transaction(fromAccountId, toAccountId, amount, description, dateOfTransaction);
+    }
+
+    //This method needs a better string representation of the transaction
+    @Override
+    public String toString() {
+        return "Transaction{" + "fromAccountId=" + fromAccountId + ", toAccountId=" + toAccountId + ", amount=" + amount + ", description=" + description + ", dateOfTransaction=" + dateOfTransaction + '}';
+    }
+
 }
