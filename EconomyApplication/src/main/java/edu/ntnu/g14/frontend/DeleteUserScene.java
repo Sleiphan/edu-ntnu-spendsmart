5e670016bc9338ae4d53d6aa49961ec8596cecf5
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2023-04-08 19:29:24 +0200

    resize controller elements in DeleteUserScene

--- EconomyApplication/src/main/java/edu/ntnu/g14/frontend/DeleteUserScene.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/frontend/DeleteUserScene.java
@@ -1,37 +1,54 @@
 package edu.ntnu.g14.frontend;
 
 import java.io.FileNotFoundException;
+import java.util.WeakHashMap;
 
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.Paint;
 import javafx.scene.text.Text;
+import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 public class DeleteUserScene {
+
+    private VBox vBox;
+
+    private StackPane stackPane;
     static Stage stage = ApplicationFront.getStage();
 
     static public Scene scene() throws FileNotFoundException {
-        Text loggedInUser = ApplicationObjects.newText("Walter Banks", 60, false, 600, 150);
-        Text loggedInUserEmail = ApplicationObjects.newText("walBa76@gmail.com", 20, false, 685, 200);
+        Text loggedInUser = ApplicationObjects.newText("Walter Banks", 40, false, 0, 0);
+        Text loggedInUserEmail = ApplicationObjects.newText("walBa76@gmail.com", 20, false, 0, 0);
+
+
+        // Create a VBox to hold loggedInUser and loggedInUserEmail
+        VBox userInfoBox = new VBox(5, loggedInUser, loggedInUserEmail);
+        userInfoBox.setAlignment(Pos.CENTER);
+        userInfoBox.setLayoutX(220); // Set the HBox's layoutX and layoutY to position it on the screen
+        userInfoBox.setLayoutY(75);
+
+
         Text warningText = ApplicationObjects.newText("Are you sure you want to delete this user?\n" +
-                "_____This action cannot be undone____\n" +
-                "___________10 seconds left__________", 20, false, 600, 350);
-
-
-        Button confirm = ApplicationObjects.newButton("Delete", 700, 520,"white", "red", 157,60,16);
+                "This action cannot be undone\n" +
+                "10 seconds left", 20, true, 0, 0);
+        warningText.setTextAlignment(TextAlignment.CENTER);
+        warningText.setVisible(false);
+        Button confirm = ApplicationObjects.newButton("Delete", 0, 0,"white", "red", 157,25,16);
         confirm.setTextFill(Paint.valueOf("WHITE"));
-        confirm.setMinWidth(157);
-        confirm.setMinHeight(50);
-        confirm.setOnMousePressed(event -> {ApplicationObjects.alertBox("Deleted user", "You have deleted user", "Walter Banks R.I.P");    confirm.setStyle("-fx-background-color: red");
+        confirm.setOnMousePressed(event -> {warningText.setVisible(true);
+            ApplicationObjects.alertBox("Deleted user", "You have deleted user", "Walter Banks R.I.P");    confirm.setStyle("-fx-background-color: red");
             confirm.setTextFill(Paint.valueOf("WHITE"));
-            confirm.setMinWidth(157);
-            confirm.setMinHeight(50);});
+        });
 
-        Button cancel = ApplicationObjects.newButton("Cancel", 700, 570,"white", "grey", 157,60,16);
+        Button cancel = ApplicationObjects.newButton("Cancel", 0, 0,"white", "grey", 157,25,16);
         cancel.setOnAction(event -> {
             try {
                 stage.setScene(UserManagementScene.scene());
@@ -40,6 +57,11 @@ public class DeleteUserScene {
                 e.printStackTrace();
             }
         });
+        // Create an HBox to hold warningText, confirm and cancel
+        VBox deleteUserBox = new VBox(5, warningText, confirm, cancel);
+        deleteUserBox.setAlignment(Pos.CENTER);
+        deleteUserBox.setLayoutX(180); // Set the VBox's layoutX and layoutY to position it on the screen
+        deleteUserBox.setLayoutY(350);
 
         
         ImageView homeButton = ApplicationObjects.newImage("home.png", 10, 10, 20, 20);
@@ -53,7 +75,7 @@ public class DeleteUserScene {
         Button dropDownButton = ApplicationObjects.newButton("test", 676, 10, "black", "white", 10, 10, 10);
         Group dropDown = ApplicationObjects.dropDownMenu();
         ImageView manageUserButton = ApplicationObjects.newImage("user.png", 646, 10, 20, 20);
-        Group root = new Group(loggedInUser, loggedInUserEmail, cancel, confirm, warningText, dropDownButton, homeButton, manageUserButton);
+        Group root = new Group(userInfoBox, deleteUserBox, dropDownButton, homeButton, manageUserButton);
         dropDownButton.setOnAction(e -> {
             root.getChildren().add(dropDown);
         });
