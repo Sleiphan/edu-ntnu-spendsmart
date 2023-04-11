378b1bd6e99b4ef4d42a9818357421f4eb20815f
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-04-11 19:34:55 +0200

    Refactor indentation, change field names and datatype of amount to BigDecimal

--- EconomyApplication/src/main/java/edu/ntnu/g14/Transaction.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/Transaction.java
@@ -1,45 +1,46 @@
 package edu.ntnu.g14;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Objects;
 
 public class Transaction {
     private static final DateTimeFormatter dateFormatter = 
     DateTimeFormatter.ofPattern("dd/MM/yyyy");
-  //These private fields should maybe be public instead?
-    private String fromAccountId;
-    private String toAccountId;
-    private short amount;
-    private String description;
+    private final String fromAccountNumber;
+    private final String toAccountNumber;
+    private final BigDecimal amount;
+    private final String description;
 
-    private LocalDate dateOfTransaction;
+    private final LocalDate dateOfTransaction;
 
-    public Transaction(String fromAccountId, String toAccountId, short amount, String description, LocalDate dateOfTransaction) {
-      if (fromAccountId.isEmpty())
-        throw new IllegalArgumentException("From account ID cannot be empty");
-      if (toAccountId.isEmpty())
-        throw new IllegalArgumentException("To account ID cannot be empty");
-      if (description == null)
-        throw new IllegalArgumentException("Description cannot be null");
-      if (dateOfTransaction == null)
-        throw new IllegalArgumentException("Date of transaction cannot be null");
+    public Transaction(String fromAccountNumber, String toAccountNumber, BigDecimal amount, String description, LocalDate dateOfTransaction) {
+        if (fromAccountNumber.isEmpty())
+            throw new IllegalArgumentException("From account ID cannot be empty");
+        if (toAccountNumber.isEmpty())
+            throw new IllegalArgumentException("To account ID cannot be empty");
+        if (description == null)
+            throw new IllegalArgumentException("Description cannot be null");
+        if (dateOfTransaction == null)
+            throw new IllegalArgumentException("Date of transaction cannot be null");
 
-        this.fromAccountId = fromAccountId;
-        this.toAccountId = toAccountId;
+        this.fromAccountNumber = fromAccountNumber;
+        this.toAccountNumber = toAccountNumber;
         this.amount = amount;
         this.description = description;
         this.dateOfTransaction = dateOfTransaction;
     }
 
-    public String getFromAccountId() {
-        return fromAccountId;
+    public String getFromAccountNumber() {
+        return fromAccountNumber;
     }
 
-    public String getToAccountId() {
-        return toAccountId;
+    public String getToAccountNumber() {
+        return toAccountNumber;
     }
 
-    public short getAmount() {
+    public BigDecimal getAmount() {
         return amount;
     }
 
@@ -51,72 +52,49 @@ public class Transaction {
         return dateOfTransaction;
     }
 
-    public void setFromAccountId(String fromAccountId) {
-        this.fromAccountId = fromAccountId;
-    }
-
-    public void setToAccountId(String toAccountId) {
-        this.toAccountId = toAccountId;
-    }
-
-    public void setAmount(short amount) {
-        this.amount = amount;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public void setDateOfTransaction(LocalDate dateOfTransaction) {
-        this.dateOfTransaction = dateOfTransaction;
-    }
 
     //This method needs a better string representation of the transaction
     @Override
     public String toString() {
-        return "Transaction{" + "fromAccountId=" + fromAccountId + ", toAccountId=" + toAccountId + ", amount=" + amount + ", description=" + description + ", dateOfTransaction=" + dateOfTransaction + '}';
+        return "Transaction{" + "fromAccountId=" + fromAccountNumber + ", toAccountId=" + toAccountNumber + ", amount=" + amount + ", description=" + description + ", dateOfTransaction=" + dateOfTransaction + '}';
     }
 
     @Override
     public boolean equals(Object o) {
-      if (!(o instanceof Transaction))
-        return false;
+        if (!(o instanceof Transaction))
+            return false;
 
-      Transaction t = (Transaction) o;
-      if (amount != t.amount)
-        return false;
-      if (!toAccountId.equals(t.toAccountId))
-        return false;
-      if (!fromAccountId.equals(t.fromAccountId))
-        return false;
-      if (!description.equals(t.description))
-        return false;
-      if (!dateOfTransaction.equals(t.dateOfTransaction))
-        return false;
-
-      return true;
+        Transaction t = (Transaction) o;
+        if (!Objects.equals(amount, t.amount))
+            return false;
+        if (!toAccountNumber.equals(t.toAccountNumber))
+            return false;
+        if (!fromAccountNumber.equals(t.fromAccountNumber))
+            return false;
+        if (!description.equals(t.description))
+            return false;
+        return dateOfTransaction.equals(t.dateOfTransaction);
     }
 
     public static final String CSV_FIELD_DELIMITER = ";";
     public String toCSVString() {
-      String sb =
-              dateOfTransaction.format(dateFormatter) +
-              amount + CSV_FIELD_DELIMITER +
-              toAccountId + CSV_FIELD_DELIMITER +
-              fromAccountId + CSV_FIELD_DELIMITER +
-              description + CSV_FIELD_DELIMITER;
-
-      return sb;
+        String sb =
+                dateOfTransaction.format(dateFormatter) +
+                amount + CSV_FIELD_DELIMITER +
+                        toAccountNumber + CSV_FIELD_DELIMITER +
+                        fromAccountNumber + CSV_FIELD_DELIMITER +
+                description + CSV_FIELD_DELIMITER;
+        return sb;
     }
 
     public static Transaction fromCSVString(String csvString) {
-      String[] fields = csvString.split(CSV_FIELD_DELIMITER);
-      String fromAccountId   = fields[3];
-      String toAccountId     = fields[2];
-      short amount           = Short.parseShort(fields[1]);
-      String description     = fields[4];
-      LocalDate dateOfTransaction = LocalDate.parse(fields[0], dateFormatter);
+        String[] fields = csvString.split(CSV_FIELD_DELIMITER);
+        String fromAccountId   = fields[3];
+        String toAccountId     = fields[2];
+        BigDecimal amount      = new BigDecimal(fields[1]);
+        String description     = fields[4];
+        LocalDate dateOfTransaction = LocalDate.parse(fields[0], dateFormatter);
 
-      return new Transaction(fromAccountId, toAccountId, amount, description, dateOfTransaction);
+        return new Transaction(fromAccountId, toAccountId, amount, description, dateOfTransaction);
     }
 }
