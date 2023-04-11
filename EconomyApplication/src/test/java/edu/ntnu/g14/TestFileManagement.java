1488ae10b207a7f8c2dfcbfdb878ab119bfe6fc9
olavsro <olavsie@hotmail.com>
2023-04-11 16:44:28 +0200

    made changes to txt

--- EconomyApplication/src/test/java/edu/ntnu/g14/TestFileManagement.java
+++ EconomyApplication/src/test/java/edu/ntnu/g14/TestFileManagement.java
@@ -6,9 +6,9 @@ import java.time.LocalDate;
 import org.junit.jupiter.api.Test;
 
 class TestFileManagement {
-    Account testAccount = new Account(AccountCategory.SAVINGS_ACCOUNT, BigDecimal.valueOf(100000), "1232.232.12322", "kortkonto");
-    Invoice testInvoice = new Invoice(LocalDate.of(2023, 12, 4), BigDecimal.valueOf(100000), "1232.232.12322");
-    Transaction testTransaction = new Transaction("1232.232.12322", "1244.232.12322", Short.parseShort("344"), "brukte penger", LocalDate.of(2023, 12, 4));
+    Account testAccount = new Account(AccountCategory.SAVINGS_ACCOUNT, BigDecimal.valueOf(100000), "1256.65.56605", "kortkonto");
+    Invoice testInvoice = new Invoice(LocalDate.of(2023, 12, 4), BigDecimal.valueOf(100000), "1256.65.56605");
+    Transaction testTransaction = new Transaction("1256.65.56605", "1256.65.56605", Short.parseShort("344"), "brukte penger", LocalDate.of(2023, 12, 4));
     Budget testBudget = new Budget(Byte.parseByte("90"), GenderCategory.FEMALE);
     Login loginInfo = new Login("test", "test", "Test#1");
     User testUser = new User(null, null, loginInfo, null, null, null, null, testBudget);
@@ -22,7 +22,7 @@ class TestFileManagement {
         FileManagement.writeNewBudget("olav#1", testBudget);
 
         System.out.println(FileManagement.readAllTransactions("olav#1").toString());
-        System.out.println(FileManagement.readLatestTransactions("olav#1", 3).toString());
+        System.out.println(FileManagement.readLatestTransactions("olav#1", 1).toString());
         System.out.println(FileManagement.getInvoicesForUser("olav#1").toString());
         
     }
