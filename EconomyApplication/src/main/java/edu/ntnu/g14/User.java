b8a285e3cf52999431594af5a938b016e4594328
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-04-12 14:15:18 +0200

    Make fields accounts and transactions of type List<Account> and List<Transaction>

--- EconomyApplication/src/main/java/edu/ntnu/g14/User.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/User.java
@@ -1,32 +1,48 @@
 package edu.ntnu.g14;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 
 public class User extends Personalia {
-    private Account[] accounts;
-    private Invoice[] invoices;
-    private Transaction[] transactions;
-    private Budget budget;
+    private final List<Account> accounts;
+    private final Invoice[] invoices;
+    private final List<Transaction> transactions;
+    private final Budget budget;
 
     public User(Account[] accounts, Invoice[] invoices,
         Login loginInfo, String email, String lastName, String firstName,
         Transaction[] transactions, Budget budget) {
-        super(loginInfo,email,lastName,firstName);
-        this.transactions=transactions;
-        this.accounts=accounts;
-        this.invoices=invoices;
+        super(loginInfo, email, lastName, firstName);
+        this.transactions = new ArrayList<>();
+
+        this.invoices = invoices;
         this.budget = budget;
+        this.accounts = new ArrayList<>();
+        if (transactions != null) {
+            this.transactions.addAll(List.of(transactions));
+        }
+        if (accounts != null) {
+            this.accounts.addAll(List.of(accounts));
+        }
     }
 
-    public Account[] getAllAccounts() {
-        return accounts;
+    public Account[] getAccounts() {
+        return this.accounts.toArray(Account[]::new);
+    }
+    public List<Account> getAccountsAsList() {
+        return this.accounts;
+    }
+    public void addAccount(Account account) {
+        this.accounts.add(account);
     }
     public Invoice[] getAllInvoices() {
         return invoices;
     }
-
     public Transaction[] getTransactions(){
-        return transactions;
+        return this.transactions.toArray(Transaction[]::new);
+    }
+    public List<Transaction> getTransactionsAsList(){
+        return this.transactions;
     }
 
     public Budget getBudget(){
