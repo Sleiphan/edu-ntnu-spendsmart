0a81f33b218181619746ac99c48bd075a58e7229
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2023-04-13 15:53:57 +0200

    divide BudgetingScene into more methods

--- EconomyApplication/src/main/java/edu/ntnu/g14/frontend/BudgetingScene.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/frontend/BudgetingScene.java
@@ -2,7 +2,6 @@ package edu.ntnu.g14.frontend;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.math.BigDecimal;
 import java.util.Map;
 
 import edu.ntnu.g14.Budget;
@@ -11,7 +10,6 @@ import edu.ntnu.g14.User;
 import edu.ntnu.g14.dao.BudgetDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -23,62 +21,112 @@ import javafx.stage.Stage;
 public class BudgetingScene {
     static Stage stage = ApplicationFront.getStage();
     private static User loggedInUser = ApplicationFront.loggedInUser;
-
     static BudgetDAO budgetDAO;
 
     static public Scene scene() throws IOException {
+        budgetDAO = getBudgetDAO();
+
+        TableView<ObservableList<Object>> revenues = createRevenuesTable();
+        TableView<ObservableList<Object>> expenditures = createExpendituresTable();
+
+        Label backgroundLabel = createBackgroundLabel();
+        Text MonthlyBudget = createMonthlyBudgetText();
+        Text savings = createSavingsText();
+
+        Button createNewBudget = createCreateNewBudgetButton();
+        Button budgetSuggestions = createBudgetSuggestionsButton();
+
+        ImageView homeButton = createHomeButton();
+        Button dropDownButton = createDropDownButton();
+        Group dropDown = ApplicationObjects.dropDownMenu();
+        ImageView manageUserButton = createManageUserButton();
+
+        Group root = new Group(backgroundLabel, revenues, expenditures, createNewBudget,
+                budgetSuggestions, MonthlyBudget, savings, dropDownButton, homeButton, manageUserButton);
+        dropDownButton.setOnAction(e -> root.getChildren().add(dropDown));
+
+        Scene scene = createScene(root);
+
+        Group userButtons = ApplicationObjects.userMenu();
+        addUserButtonsEventHandler(manageUserButton, root, userButtons);
+        scene.setOnMouseClicked(e -> {
+            root.getChildren().remove(userButtons);
+            root.getChildren().remove(dropDown);
+        });
+
+        return scene;
+    }
+
+    private static BudgetDAO getBudgetDAO() throws IOException {
         try {
-            budgetDAO = new BudgetDAO("budgets.txt");
+            return new BudgetDAO("saves/budgets.txt");
         } catch (IOException e) {
             e.printStackTrace();
+            throw e;
         }
+    }
+
+    private static TableView<ObservableList<Object>> createRevenuesTable() throws IOException {
         TableView<ObservableList<Object>> revenues = ApplicationObjects.newTableView(new String[]{"Revenues", "Amount"}, 40, 70, 380, 120);
-        ObservableList<ObservableList<Object>> revenuesData = initializeRevenuesData();
-        revenues.setItems(revenuesData);
+        revenues.setItems(initializeRevenuesData());
         revenues.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         revenues.setFixedCellSize(50);
+        return revenues;
+    }
 
+    private static TableView<ObservableList<Object>> createExpendituresTable() {
         TableView<ObservableList<Object>> expenditures = ApplicationObjects.newTableView(new String[]{"Expenditures", "Budget"}, 40, 210, 380, 250);
-        ObservableList<ObservableList<Object>> expenditureData = initializeExpenditureData();
-        expenditures.setItems(expenditureData);
+        expenditures.setItems(initializeExpenditureData());
         expenditures.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY);
         expenditures.setFixedCellSize(50);
+        return expenditures;
+    }
 
+    private static Label createBackgroundLabel() {
         Label backgroundLabel = new Label();
         backgroundLabel.setPrefWidth(400);
         backgroundLabel.setPrefHeight(500);
         backgroundLabel.setLayoutX(30);
         backgroundLabel.setLayoutY(40);
         backgroundLabel.setStyle("-fx-background-color: #f0f0f0;");
+        return backgroundLabel;
+    }
 
-        Text MonthlyBudget = ApplicationObjects.newText("Monthly Budget", 30, false, 40, 60);
-        MonthlyBudget.setStyle("-fx-font-size: 18px; -fx-font-weight: bold;");
+    private static Text createMonthlyBudgetText() {
+        return ApplicationObjects.newText("Monthly Budget", 30, false, 40, 60);
+    }
 
-        Text savings = ApplicationObjects.newText("Savings: 3000", 30, false, 40, 480);
+    private static Text createSavingsText() {
+        Text savings = ApplicationObjects.newText("Savings: " + loggedInUser.getBudget().getSavings(), 30, false, 40, 480);
         savings.setStyle("-fx-font-size: 14px; -fx-font-weight: bold;");
+        return savings;
+    }
 
+    private static Button createCreateNewBudgetButton() {
         Button createNewBudget = ApplicationObjects.newButton("Create new budget", 470, 180, 157, 25, 16);
-        Button budgetSuggestions = ApplicationObjects.newButton("Budget suggestion", 470, 270, 157, 25, 16);
-
         createNewBudget.setOnAction(e -> {
             try {
                 stage.setScene(CreateNewBudgetScene.scene());
             } catch (IOException e1) {
-                
                 e1.printStackTrace();
             }
         });
+        return createNewBudget;
+    }
+
+    private static Button createBudgetSuggestionsButton() {
+        Button budgetSuggestions = ApplicationObjects.newButton("Budget suggestion", 470, 270, 157, 25, 16);
         budgetSuggestions.setOnAction(e -> {
             try {
                 stage.setScene(BudgetSuggestionsScene.scene());
             } catch (IOException e1) {
-
                 e1.printStackTrace();
             }
         });
+        return budgetSuggestions;
+    }
 
-        
-
+    private static ImageView createHomeButton() throws FileNotFoundException {
         ImageView homeButton = ApplicationObjects.newImage("home.png", 10, 10, 20, 20);
         homeButton.setOnMouseClicked(e -> {
             try {
@@ -87,34 +135,29 @@ public class BudgetingScene {
                 e1.printStackTrace();
             }
         });
-        Button dropDownButton = ApplicationObjects.newButton("test", 676, 10, 10, 10, 10);
-        Group dropDown = ApplicationObjects.dropDownMenu();
-        ImageView manageUserButton = ApplicationObjects.newImage("user.png", 646, 10, 20, 20);
-        Group root = new Group(backgroundLabel, revenues, expenditures, createNewBudget,
-                budgetSuggestions, MonthlyBudget, savings, dropDownButton, homeButton, manageUserButton);
-        dropDownButton.setOnAction(e -> {
-            root.getChildren().add(dropDown);
-        });
+        return homeButton;
+    }
 
+    private static Button createDropDownButton() {
+        return ApplicationObjects.newButton("test", 676, 10, 10, 10, 10);
+    }
+
+    private static ImageView createManageUserButton() throws FileNotFoundException {
+        return ApplicationObjects.newImage("user.png", 646, 10, 20, 20);
+    }
+
+    private static Scene createScene(Group root) {
         Scene scene = new Scene(root, 728, 567, ApplicationObjects.getSceneColor());
-        
-    
-        Group userButtons = ApplicationObjects.userMenu();
-        manageUserButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event){
-                root.getChildren().add(userButtons);
-                event.consume();
-            }
-        });
-        scene.setOnMouseClicked(e -> {
-            root.getChildren().remove(userButtons);
-            root.getChildren().remove(dropDown);
-        });
         scene.setFill(ApplicationObjects.getSceneColor());
         return scene;
     }
 
+    private static void addUserButtonsEventHandler(ImageView manageUserButton, Group root, Group userButtons) {
+        manageUserButton.addEventHandler(MouseEvent.MOUSE_CLICKED, event -> {
+            root.getChildren().add(userButtons);
+            event.consume();
+        });
+    }
     public static ObservableList<ObservableList<Object>> initializeRevenuesData() throws IOException {
         ObservableList<ObservableList<Object>> revenuesData = FXCollections.observableArrayList();
         Budget budget = budgetDAO.getBudget(loggedInUser.getLoginInfo().getUserId());
@@ -122,7 +165,9 @@ public class BudgetingScene {
         if (budget != null) {
             for (BudgetItem entry : budget.getEntries()) {
                 // need to update budgetitem to contain getRevenues category
-                revenuesData.add(FXCollections.observableArrayList(entry.getCategory() , entry.getFinancialValue()));
+                if (entry.getCategory().getType().equals("r")) {
+                    revenuesData.add(FXCollections.observableArrayList(entry.getCategory(), entry.getFinancialValue()));
+                }
             }
         }
 
@@ -135,13 +180,13 @@ public class BudgetingScene {
         // Fetch expenditures from budget and add them to expenditureData
         if (budget != null) {
             for (BudgetItem entry : budget.getEntries()) {
-                expenditureData.add(FXCollections.observableArrayList(entry.getCategory(), entry.getFinancialValue()));
+                if (entry.getCategory().getType().equals("e")) {
+                    expenditureData.add(FXCollections.observableArrayList(entry.getCategory(), entry.getFinancialValue()));
+                }
             }
         }
-
         return expenditureData;
     }
 
-
-
 }
+
