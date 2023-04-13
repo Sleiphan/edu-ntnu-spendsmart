696c92daa7cdd9986e6123641b14c22c2822669e
Valdemar Åstorp Beere <valdemb@stud.ntnu.no>
2023-04-13 15:52:38 +0200

    add type to BudgetCategory

--- EconomyApplication/src/main/java/edu/ntnu/g14/BudgetCategory.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/BudgetCategory.java
@@ -1,15 +1,25 @@
 package edu.ntnu.g14;
-public enum BudgetCategory {
-    FOOD_AND_DRINK,
-    CLOTHES_AND_SHOES,
-    PERSONAL_CARE,
-    LEISURE,
-    TRAVEL,
-    ALCOHOL_AND_TOBACCO,
-    OTHER,
-    SALARY,
-    PAYMENT,
-    INCOME,
-    BUSINESS
 
+public enum BudgetCategory {
+    FOOD_AND_DRINK("e"),
+    CLOTHES_AND_SHOES("e"),
+    PERSONAL_CARE("e"),
+    LEISURE("e"),
+    TRAVEL("e"),
+    ALCOHOL_AND_TOBACCO("e"),
+    OTHER("e"),
+    SALARY("r"),
+    PAYMENT("r"),
+    INCOME("r"),
+    BUSINESS("r");
+
+    private final String type;
+
+    BudgetCategory(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
