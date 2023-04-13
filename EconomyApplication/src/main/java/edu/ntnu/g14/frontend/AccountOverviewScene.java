2867c9c52bc51146b2fc46f6ea53777a9c722c85
Yasin Ali Marouga <yasinam@stud.ntnu.no>
2023-04-14 00:45:20 +0200

    Add new 'Edit Account' button and partially implement functionality

--- EconomyApplication/src/main/java/edu/ntnu/g14/frontend/AccountOverviewScene.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/frontend/AccountOverviewScene.java
@@ -44,22 +44,26 @@ public class AccountOverviewScene {
         String[] columnTitlesTransactionsTable = {"Transaction", "Date", "Amount"};
 
 
-        ChoiceBox<String> accountChoiceBox = ApplicationObjects.newChoiceBox(columnTitlesTransactionsTable, 364, 30, 30, 364-(364/2), 30);
-        accountChoiceBox.setItems(accountNames);
-        accountChoiceBox.setValue(accounts.get(0).getAccountName());
-        Text accountNumberText = ApplicationObjects.newText("Account Number: " + accounts.get(0).getAccountNumber(), 14, false, 266, 130);
-        Text amountText = ApplicationObjects.newText("Amount: " + accounts.get(0).getAmount() + "kr", 20, false, 290, 160);
-        Button addTransaction = ApplicationObjects.newButton("Add Transaction", 20, 30, 120, 20, 14);
-        Button addAccount = ApplicationObjects.newButton("Add Account", 20, 60, 120, 20, 14);
+        ComboBox<String> accountComboBox = ApplicationObjects.newComboBox(columnTitlesTransactionsTable, 364, 30, 30, 364-(364/2), 50);
+        accountComboBox.setItems(accountNames);
+        accountComboBox.setValue(accounts.get(0).getAccountName());
+        Text accountNumberText    = ApplicationObjects.newText("Account Number: " + accounts.get(0).getAccountNumber(), 14, false, 0, 130);
+        Text amountText           = ApplicationObjects.newText("Amount: " + accounts.get(0).getAmount() + "kr", 20, false, 0, 160);
+        Button addExpense         = ApplicationObjects.newButton("Add Expense", 30,80, 100, 20,14);
+        Button addIncome          = ApplicationObjects.newButton("Add Income", 30,50, 100, 20,14);
+        Button addAccount         = ApplicationObjects.newButton("Add Account", 728 - 130, 50, 100, 20, 14);
+        Button editAccount        = ApplicationObjects.newButton("Edit Account", 728 - 130, 80, 100, 20, 14);
         Text lastTransactionsText = ApplicationObjects.newText("Last Transactions:", 24, false, 20, 200);
+        amountText.setLayoutX((double) 728/2 - amountText.getLayoutBounds().getWidth()/2);
+        accountNumberText.setLayoutX((double) 728/2 - accountNumberText.getLayoutBounds().getWidth()/2);
         lastTransactionsTable = ApplicationObjects.newTableView(columnTitlesTransactionsTable, 20, 230, 688, 300);
         lastTransactionsTable.setColumnResizePolicy(TableView.CONSTRAINED_RESIZE_POLICY_ALL_COLUMNS);
-        setCurrentAccount(accounts, accountChoiceBox);
+        setCurrentAccount(accountComboBox.getValue());
         ObservableList<ObservableList<Object>> lastTransactionsData = initializeLastTransactionsData(currentAccount);
         lastTransactionsTable.setItems(lastTransactionsData);
 
-        accountChoiceBox.setOnAction(actionEvent -> {
-            setCurrentAccount(accounts, accountChoiceBox);
+        accountComboBox.setOnAction(actionEvent -> {
+            setCurrentAccount(accountComboBox.getValue());
 
 
             accountNumberText.setText("Account Number: " + currentAccount.getAccountNumber());
@@ -73,35 +77,24 @@ public class AccountOverviewScene {
                 createAccountDialog(accountNames, result);
             }
         });
-        addTransaction.setOnAction(actionEvent -> {
-            Dialog<TransactionWithProperty> transactionWithPropertyDialog =
-                    new ApplicationObjects.TransactionWithPropertyDialog(
-                            new TransactionWithProperty(null,
-                                    null, null,
-                                    null, null, null));
-            Optional<TransactionWithProperty> result = transactionWithPropertyDialog.showAndWait();
+        editAccount.setOnAction(actionEvent -> {
+            Dialog<Account> accountDialog = new ApplicationObjects.EditAccountDialog(currentAccount);
+            Optional<Account> result = accountDialog.showAndWait();
             if (result.isPresent()) {
-                TransactionWithProperty transactionWithProperty = result.get();
-                Transaction transaction = new Transaction(transactionWithProperty.getFromAccountId(),
-                        transactionWithProperty.getToAccountId(), new BigDecimal(transactionWithProperty.getAmount()),
-                        transactionWithProperty.getDescription(), transactionWithProperty.getDateOfTransaction(),
-                        BudgetCategory.valueOf(transactionWithProperty.getCategory()));
+                Account account = result.get();
+                accountComboBox.setItems(FXCollections.observableArrayList(getAccountsNames()));
 
-                FileManagement.writeTransaction(ApplicationFront.loggedInUser.getLoginInfo().getUserId(), transaction);
-                ApplicationFront.loggedInUser.getTransactionsAsList().add(transaction);
-                addTransaction(transaction);
             }
         });
-        Button back_bt = ApplicationObjects.newButton("Back", 20, 90, 120, 20, 14);
-        back_bt.setOnAction(e -> {
-            try {
-                stage.setScene(MainPageScene.scene());
-            } catch (IOException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
+
+        addExpense.setOnAction(actionEvent -> {
+            showDialog(false);
         });
-    
+
+        addIncome.setOnAction(actionEvent -> {
+            showDialog(true);
+        });
+
 
 
         ImageView homeButton = ApplicationObjects.newImage("home.png", 10, 10, 20, 20);
@@ -116,7 +109,7 @@ public class AccountOverviewScene {
         Button dropDownButton = ApplicationObjects.newButton("test", 676, 10, 10, 10, 10);
         Group dropDown = ApplicationObjects.dropDownMenu();
         ImageView manageUserButton = ApplicationObjects.newImage("user.png", 646, 10, 20, 20);
-        Group root = new Group(accountChoiceBox, accountNumberText, amountText, addTransaction, addAccount, lastTransactionsText, lastTransactionsTable, dropDownButton, homeButton, manageUserButton);
+        Group root = new Group(accountComboBox, accountNumberText, amountText, addIncome, addExpense, addAccount, editAccount, lastTransactionsText, lastTransactionsTable, dropDownButton, homeButton, manageUserButton);
         dropDownButton.setOnAction(e -> root.getChildren().add(dropDown));
 
         root.getStylesheets().add("StyleSheet.css");
@@ -147,18 +140,16 @@ public class AccountOverviewScene {
         ApplicationFront.loggedInUser.addAccount(account);
     }
 
-    private static void setCurrentAccount(List<Account> accounts, ChoiceBox<String> accountChoiceBox) {
-        currentAccount = accounts.stream().filter(account -> account
-                .getAccountName()
-                .equals(accountChoiceBox.getValue()))
-                .findFirst().orElseThrow(() -> new IllegalStateException("Account could not be found."));
+    private static void setCurrentAccount(String accountName) {
+        currentAccount = ApplicationFront.loggedInUser.getAccountWithAccountName(accountName);
         initializeLastTransactionsData(currentAccount);
         lastTransactionsTable.setItems(lastTransactionsData);
 
     }
-    private static String[] getAccountsNames() {
-        return Arrays.stream(ApplicationFront.loggedInUser.getAccounts())
-                .map(Account::getAccountName).toArray(String[]::new);
+    private static List<String> getAccountsNames() {
+
+        return ApplicationFront.loggedInUser.getAccountsAsList()
+                .stream().map(Account::getAccountName).collect(Collectors.toList());
     }
     private static ObservableList<ObservableList<Object>> initializeLastTransactionsData(Account account) {
 
@@ -196,6 +187,36 @@ public class AccountOverviewScene {
             lastTransactionsData.add(FXCollections.observableArrayList(transaction.getFromAccountNumber(),
                     transaction.getDateOfTransaction().format(ApplicationObjects.dateFormatter),
                     transaction.getAmount()));
+        }
+    }
+    private static void showDialog(Boolean incomeOrExpense) {
+        Dialog<TransactionWithProperty> transactionWithPropertyDialog =
+                new ApplicationObjects.TransactionWithPropertyDialog(
+                        new TransactionWithProperty(null,
+                                null, null,
+                                null, null, null), incomeOrExpense);
+        Optional<TransactionWithProperty> result = transactionWithPropertyDialog.showAndWait();
+        if (result.isPresent()) {
+            TransactionWithProperty transactionWithProperty = result.get();
+            Transaction transaction;
+            if (incomeOrExpense) {
+                transaction = new Transaction(transactionWithProperty.getFromAccountId(),
+                        ApplicationFront.loggedInUser.getAccountWithAccountName(transactionWithProperty.getToAccountId()).getAccountNumber(),
+                        new BigDecimal(transactionWithProperty.getAmount()),
+                        transactionWithProperty.getDescription(), transactionWithProperty.getDateOfTransaction(),
+                        BudgetCategory.valueOf(transactionWithProperty.getCategory()));
+            } else {
+                transaction = new Transaction(
+                        ApplicationFront.loggedInUser
+                        .getAccountWithAccountName(transactionWithProperty.getFromAccountId()).getAccountNumber(),
+                        transactionWithProperty.getToAccountId(), new BigDecimal(transactionWithProperty.getAmount()),
+                        transactionWithProperty.getDescription(), transactionWithProperty.getDateOfTransaction(),
+                        BudgetCategory.valueOf(transactionWithProperty.getCategory()));
+            }
+
+            FileManagement.writeTransaction(ApplicationFront.loggedInUser.getLoginInfo().getUserId(), transaction);
+            ApplicationFront.loggedInUser.getTransactionsAsList().add(transaction);
+            addTransaction(transaction);
         }
     }
 }
