1b6cb7403b48abdeb5b82b33aa84fc237ffc95bb
olavsro <olavsie@hotmail.com>
2023-04-25 16:00:31 +0200

    added javadoc to transactionDAO

--- EconomyApplication/src/main/java/edu/ntnu/g14/dao/TransactionDAO.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/dao/TransactionDAO.java
@@ -16,16 +16,33 @@ public class TransactionDAO {
     private final IndexedDataFile file;
     private final  Charset charset;
 
+    /**
+    * Constructs a new TransactionDAO object with the given file path and default charset.
+    * @param filePath the file path of the IndexedDataFile
+    * @throws IOException if there is an error creating the IndexedDataFile
+    */
     public TransactionDAO(String filePath) throws IOException {
         this(filePath, Charset.defaultCharset());
     }
 
+    /**
+    * Constructs a new TransactionDAO object with the given file path and charset.
+    * @param filePath the file path of the IndexedDataFile
+    * @param charset the charset used for encoding/decoding transaction data
+    * @throws IOException if there is an error creating the IndexedDataFile
+    */
     public TransactionDAO(String filePath, Charset charset) throws IOException {
         this.file = new IndexedDataFile(filePath, charset);
         this.charset = charset;
     }
 
-
+    /**
+    * Adds a new transaction for the given user.
+    * @param userID the user ID
+    * @param obj the Transaction object to add
+    * @throws IOException if there is an error adding the transaction to the IndexedDataFile
+    * @throws IllegalArgumentException if the transaction object is null
+    */
     public void addNewTransaction(String userID, Transaction obj) throws IOException {
         if (obj == null)
             throw new IllegalArgumentException("Parameter transaction cannot be null");
@@ -35,6 +52,13 @@ public class TransactionDAO {
         file.addNewData(userID, data);
     }
 
+    /**
+    * Retrieves all transactions for the given user.
+    * @param userID the user ID
+    * @return an array of Transaction objects for the user, sorted by time with the latest transaction at the last index,
+    * or null if no transactions were found for the user
+    * @throws IOException if there is an error reading the transactions from the IndexedDataFile
+    */
     public Transaction[] getAllTransactionsForUser(String userID) throws IOException {
         byte[][] data = file.readAllInIdentifier(userID);
         if (data == null)
@@ -50,12 +74,13 @@ public class TransactionDAO {
     }
 
     /**
-     * Attempts to read the latest 'amount' number of Transactions, and returns them as
-     * @param userID
-     * @param amount
-     * @return
-     * @throws IOException
-     */
+    * Retrieves the latest 'amount' number of transactions for the given user.
+    * @param userID the user ID
+    * @param amount the number of transactions to retrieve
+    * @return an array of the latest 'amount' number of Transaction objects for the user, sorted by time with the latest transaction at the last index,
+    * or null if no transactions were found for the user
+    * @throws IOException if there is an error reading the transactions from the IndexedDataFile
+    */
     public Transaction[] getLatest(String userID, int amount) throws IOException {
         byte[][] data = file.getEntriesBackwards(userID, amount);
         if (data == null)
@@ -67,6 +92,11 @@ public class TransactionDAO {
                 .toArray(Transaction[]::new);
     }
 
+    /**
+     * Deletes all transactions associated with the specified user from the data file.
+     * @param userID the identifier of the user whose transactions should be deleted
+     * @throws IOException if an I/O error occurs while attempting to delete the transactions
+     */
     public void deleteUser(String userID) throws IOException {
         file.deleteIdentifier(userID);
     }
