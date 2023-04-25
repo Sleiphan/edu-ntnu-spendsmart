f25ea0c42893f5ddab2a7dbfad264931a86b67ab
Isak Kallestad Mandal <63557128+Warlock2358@users.noreply.github.com>
2023-04-25 14:18:09 +0200

    Added JavaDoc to classes

--- EconomyApplication/src/main/java/edu/ntnu/g14/frontend/TransferScene.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/frontend/TransferScene.java
@@ -3,35 +3,49 @@ package edu.ntnu.g14.frontend;
 import edu.ntnu.g14.BankApplication;
 import edu.ntnu.g14.BudgetCategory;
 import edu.ntnu.g14.Transaction;
-
-import java.io.IOException;
-
 import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
-
 import javafx.stage.Stage;
 
-
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-
-import static edu.ntnu.g14.FileManagement.writeTransaction;
 import static edu.ntnu.g14.BankApplication.loggedInUser;
+import static edu.ntnu.g14.FileManagement.writeTransaction;
 
+/**
+ * This class represents the scene for transferring money between accounts. It
+ * contains a text field for input of the account number to transfer from, the
+ * amount to transfer, a description of the transaction, the account number to
+ * transfer to, and a category for the transaction.
+ *
+ * @author G14
+ * @version 1.0
+ * @since 2023-04-25
+ */
 public class TransferScene {
     static Stage stage = BankApplication.getStage();
 
+    /**
+     * Creates a scene for transferring money between accounts.
+     *
+     * @return Scene
+     * @throws IOException if input is invalid
+     */
     static public Scene scene() throws IOException {
         int x = 290;
         int y = 50;
         int n = 85;
         int m = n - 5;
 
+        //Creates text fields for input
         TextField fromAccount = ApplicationObjects.newTextField("12345678910", x, y, 200, 20, 15);
         TextField amount = ApplicationObjects.newTextField("50kr", x, y + n, 100, 20, 15);
         TextField description = ApplicationObjects.newTextField("Description", x, y + 2 * n, 200, 20, 15);
@@ -41,6 +55,7 @@ public class TransferScene {
         ComboBox<String> category = ApplicationObjects.newComboBox(categoryChoices, 200, 20, 15, x, y + 4 * n - 25);
         category.setPromptText("Category of Payment");
 
+        //Creates buttons for transfer and cancel, and sets their actions
         Button transfer = ApplicationObjects.newButton("Transfer", x - 50, y + 3 * n + 125, 100, 20, 15);
         Button cancel = ApplicationObjects.newButton("Cancel", x + 70, y + 3 * n + 125, 100, 20, 15);
         transfer.setOnAction(e -> {
@@ -54,7 +69,7 @@ public class TransferScene {
                 }
                 stage.setScene(MainPageScene.scene());
             } catch (IOException e1) {
-                
+
                 e1.printStackTrace();
             }
         });
@@ -62,11 +77,11 @@ public class TransferScene {
             try {
                 stage.setScene(MainPageScene.scene());
             } catch (IOException e1) {
-                
+
                 e1.printStackTrace();
             }
         });
-        
+
         ImageView homeButton = ApplicationObjects.newImage("home.png", 10, 10, 20, 20);
         homeButton.setOnMouseClicked(e -> {
             try {
@@ -84,17 +99,19 @@ public class TransferScene {
                 ApplicationObjects.newText("Description:", 30, false, x, y + 2 * m),
                 ApplicationObjects.newText("To account:", 30, false, x, y + 3 * m),
                 transfer, cancel, dropDownButton, homeButton, manageUserButton);
+
         dropDownButton.setOnAction(e -> {
             root.getChildren().add(dropDown);
         });
+
         root.getStylesheets().add("StyleSheet.css");
         Scene scene = new Scene(root, 728, 567, ApplicationObjects.getSceneColor());
-        
-        
+
+
         Group userButtons = ApplicationObjects.userMenu();
         manageUserButton.addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
             @Override
-            public void handle(MouseEvent event){
+            public void handle(MouseEvent event) {
                 root.getChildren().add(userButtons);
                 event.consume();
             }
@@ -106,5 +123,5 @@ public class TransferScene {
 
         return scene;
     }
-    
+
 }
