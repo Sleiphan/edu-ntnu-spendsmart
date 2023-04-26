1d6ca3ae3c8165f7c7bf3ac4d960a1845a28cea8
olavsro <olavsie@hotmail.com>
2023-04-26 08:21:49 +0200

    added javadoc to budgetDAO class

--- EconomyApplication/src/main/java/edu/ntnu/g14/dao/BudgetDAO.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/dao/BudgetDAO.java
@@ -1,36 +1,46 @@
 package edu.ntnu.g14.dao;
 
 import edu.ntnu.g14.Budget;
-
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.RandomAccessFile;
-import java.nio.ByteBuffer;
-import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 
+/**
+ * The class represents a data acess object for managing budgets stored in budgets.txt
+ */
 public class BudgetDAO {
 
     private final IndexedDataFile file;
     private final Charset charset;
 
+    /**
+    * Constructs a BudgetDAO object with the specified file path.
+    *
+    * @param filePath the path to the file containing the budget data
+    * @throws IOException if an I/O error occurs while opening the file
+    */
     public BudgetDAO(String filePath) throws IOException {
         this(filePath, Charset.defaultCharset());
     }
 
+    /**
+    * Constructs a BudgetDAO object with the specified file path and character set.
+    *
+    * @param filePath the path to the file containing the budget data
+    * @param charset  the character set used to encode the budget data
+    * @throws IOException if an I/O error occurs while opening the file
+    */
     public BudgetDAO(String filePath, Charset charset) throws IOException {
         this.file = new IndexedDataFile(filePath, charset);
         this.charset = charset;
     }
 
     /**
-     *
-     * @param userID
-     * @param budget
-     * @throws IOException
-     */
+    * Sets the budget for the specified user ID.
+    *
+    * @param userID the ID of the user whose budget is being set
+    * @param budget the budget to set for the user
+    * @throws IOException if an I/O error occurs while writing to the file
+    */
     public void setBudget(String userID, Budget budget) throws IOException {
         byte[] data = budget.toCSV().getBytes(charset);
         boolean newData = file.containsIdentifier(userID);
@@ -41,6 +51,13 @@ public class BudgetDAO {
             file.addNewData(userID, data);
     }
 
+    /**
+    * Retrieves the budget for the specified user ID.
+    *
+    * @param userID the ID of the user whose budget is being retrieved
+    * @return the budget for the specified user ID, or null if no budget is found
+    * @throws IOException if an I/O error occurs while reading from the file
+    */
     public Budget getBudget(String userID) throws IOException {
         byte[] data = file.getData(userID, 0);
         if (data == null)
@@ -50,6 +67,12 @@ public class BudgetDAO {
         return Budget.fromCSV(csv);
     }
 
+    /**
+    * Deletes the budget data for the specified user ID.
+    *
+    * @param userID the ID of the user whose budget data is being deleted
+    * @throws IOException if an I/O error occurs while writing to the file
+    */
     public void deleteUser(String userID) throws IOException {
         file.deleteIdentifier(userID);
     }
