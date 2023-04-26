9fd9d367bec45793a7327683696239976f142fa0
olavsro <olavsie@hotmail.com>
2023-04-26 09:49:22 +0200

    fixed textToSpeach feature on first scene

--- EconomyApplication/src/main/java/edu/ntnu/g14/frontend/LoginChooseUserScene.java
+++ EconomyApplication/src/main/java/edu/ntnu/g14/frontend/LoginChooseUserScene.java
@@ -18,9 +18,6 @@ public class LoginChooseUserScene {
 
     static public Scene scene() throws IOException {
         MediaPlayer textToSpeach = ApplicationObjects.newSound("loginChooseUserScene");
-        if(ApplicationObjects.soundOn()){
-            textToSpeach.play();
-        }
 
         Text chooseUser = ApplicationObjects.newText("Choose user", 30, false, 0, 40);
         Text registerNew = ApplicationObjects.newText("Register new account", 10, true, 400, 280);
@@ -51,6 +48,7 @@ public class LoginChooseUserScene {
             if(soundButton.getText().equals("Sound on?") || soundButton.getText().equals("OFF")){
                 soundButton.setText("ON");
                 ApplicationObjects.setSound(true);
+                textToSpeach.play();
             } else{
                 soundButton.setText("OFF");
                 ApplicationObjects.setSound(false);
